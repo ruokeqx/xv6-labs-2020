b3e41add66a2bf036274d0d466915ae416923cdd
ruokeqx <ruokeqx@163.com>
2022-06-26 20:36:49 +0800

    barrier condition variable

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,15 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  if(++bstate.nthread < nthread){
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);  // go to sleep on cond, releasing lock mutex, acquiring upon wake up
+  }else{
+    bstate.nthread = 0;
+    bstate.round ++;
+    pthread_cond_broadcast(&bstate.barrier_cond);     // wake up every thread sleeping on cond 
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
